658d1455ee3505fd1204b31a375914a06b8e6272
Randall E. Barker <simstorm@mac.com>
2012-04-20 12:12:50 -0700

    Added camera movement. uses AWSD and arrow keys to move

--- scripts/cycles.js
+++ scripts/cycles.js
@@ -1,13 +1,16 @@
 'use strict'
-var camera, scene, renderer, objects;
-var particleLight, pointLight;
-var mesh;
-var loader = new THREE.JSONLoader();
-
-var axes = [0,0,0,0]
+var camera
+  , scene
+  , renderer
+  , objects
+  , mesh
+  , loader = new THREE.JSONLoader ()
+  , lasttimer = Date.now () * 0.0005
+  , axes = [0,0,0,0]
+  , heading = 0
   ;
 
-(function() {
+(function () {
 
 var ACode = 65
   , DCode = 68
@@ -28,14 +31,14 @@ var ACode = 65
   , downkey  = 7
   ;
 
-function updateAxes() {
+function updateAxes () {
    axes[0] = ks[akey] + ks[dkey];
    axes[1] = ks[wkey] + ks[skey];
    axes[2] = ks[leftkey] + ks[rightkey];
    axes[3] = ks[upkey] + ks[downkey];
 }
 
-window.addEventListener("keydown", function (e) {
+window.addEventListener ("keydown", function (e) {
    switch (e.keyCode) {
       case ACode: ks[akey] = -1; break;
       case DCode: ks[dkey] = 1; break;
@@ -46,10 +49,10 @@ window.addEventListener("keydown", function (e) {
       case UpArrow: ks[upkey] = 1; break;
       case DownArrow: ks[downkey] = -1; break;
    }
-   updateAxes();
+   updateAxes ();
 }, false);
 
-window.addEventListener("keyup", function (e) {
+window.addEventListener ("keyup", function (e) {
     switch (e.keyCode) {
       case ACode: ks[akey] = 0; break;
       case DCode: ks[dkey] = 0; break;
@@ -60,104 +63,102 @@ window.addEventListener("keyup", function (e) {
       case UpArrow: ks[upkey] = 0; break;
       case DownArrow: ks[downkey] = 0; break;
    }
-   updateAxes();
+   updateAxes ();
 }, false);
 
-})();
+}) ();
 
-loader.load('./models/lightcycle-purple.json', function colladaReady(collada) {
+loader.load ('./models/lightcycle-purple.json', function (node) {
 
-   mesh = new THREE.Mesh(collada, new THREE.MeshFaceMaterial())
+   mesh = new THREE.Mesh (node, new THREE.MeshFaceMaterial ())
 
-   init();
-   render();
+   init ();
+   render ();
 
 });
 
-function init() {
+function init () {
 
-   var container = document.createElement('div');
-   document.body.appendChild(container);
+   var container = document.createElement ('div');
+   document.body.appendChild (container);
 
-   scene = new THREE.Scene();
+   scene = new THREE.Scene ();
 
-   camera = new THREE.PerspectiveCamera(45, window.innerWidth / window.innerHeight, 1, 2000);
-   camera.position.set(2, 2, 3);
-   scene.add(camera);
+   camera = new THREE.PerspectiveCamera (45, window.innerWidth / window.innerHeight, 1, 2000);
+   camera.position.set (2, 2, 3);
+   scene.add (camera);
 
    // Grid
 
-   var line_material = new THREE.LineBasicMaterial({ color: 0xcccccc, opacity: 0.2 }),
-      geometry = new THREE.Geometry(),
+   var line_material = new THREE.LineBasicMaterial ({ color: 0xcccccc, opacity: 0.2 }),
+      geometry = new THREE.Geometry (),
       floor = -0.04, step = 1, size = 14;
 
    for (var i = 0; i <= size / step * 2; i ++) {
 
-      geometry.vertices.push(new THREE.Vector3(- size, floor, i * step - size));
-      geometry.vertices.push(new THREE.Vector3(size, floor, i * step - size));
+      geometry.vertices.push (new THREE.Vector3 (- size, floor, i * step - size));
+      geometry.vertices.push (new THREE.Vector3 (size, floor, i * step - size));
 
-      geometry.vertices.push(new THREE.Vector3(i * step - size, floor, -size));
-      geometry.vertices.push(new THREE.Vector3(i * step - size, floor,  size));
+      geometry.vertices.push (new THREE.Vector3 (i * step - size, floor, -size));
+      geometry.vertices.push (new THREE.Vector3 (i * step - size, floor,  size));
    }
 
-   var line = new THREE.Line(geometry, line_material, THREE.LinePieces);
-   scene.add(line);
+   var line = new THREE.Line (geometry, line_material, THREE.LinePieces);
+   scene.add (line);
 
-   scene.add(mesh);
+   scene.add (mesh);
 
    // Lights
 
-   scene.add(new THREE.AmbientLight(0x1f1f1f));
+   scene.add (new THREE.AmbientLight (0x1f1f1f));
 
-   var directionalLight = new THREE.DirectionalLight(0xffffff);
+   var directionalLight = new THREE.DirectionalLight (0xffffff);
    directionalLight.position.x = 100;
    directionalLight.position.y = 100;
    directionalLight.position.z = 0.0;
-   scene.add(directionalLight);
+   scene.add (directionalLight);
 
 
-   directionalLight = new THREE.DirectionalLight(0xffffff);
+   directionalLight = new THREE.DirectionalLight (0xffffff);
    directionalLight.position.x = -100.0;
    directionalLight.position.y = 100;
    directionalLight.position.z = 0.0;
-   scene.add(directionalLight);
+   scene.add (directionalLight);
 
 
-   renderer = new THREE.WebGLRenderer();
-   renderer.setSize(window.innerWidth, window.innerHeight);
+   renderer = new THREE.WebGLRenderer ();
+   renderer.setSize (window.innerWidth, window.innerHeight);
 
-   container.appendChild(renderer.domElement);
+   container.appendChild (renderer.domElement);
 
 /*
-   stats = new Stats();
+   stats = new Stats ();
    stats.domElement.style.position = 'absolute';
    stats.domElement.style.top = '0px';
-   container.appendChild(stats.domElement);
+   container.appendChild (stats.domElement);
 */
 
 }
 
-var lasttimer = Date.now() * 0.0005;
+function render () {
 
-function render() {
+   requestAnimationFrame (render);
 
-   requestAnimationFrame(render);
-   var timer = Date.now() * 0.0005;
-   var delta = timer - lasttimer;
+   var timer = Date.now () * 0.0005
+     , delta = timer - lasttimer
+     , TwoPi = 3.14159265 * 2
+     ;
 
-/*
-   camera.position.x = Math.cos(timer) * 5;
-   camera.position.y = 1;
-   camera.position.z = Math.sin(timer) * 5;
-*/
+   // Move the camera
+   heading -= 3.14159265 * delta * axes[2];
+   if (heading > TwoPi) { heading -= TwoPi; }
+   else if (heading < -TwoPi) { heading += TwoPi; }
+   camera.rotation.y = heading;
+   camera.position.x += delta * ((Math.cos (TwoPi - heading) * axes[0]) + (-Math.sin (heading) * axes[3])) * 10;
+   camera.position.y -= delta * axes[1] * 10;
+   camera.position.z += delta * ((Math.sin (TwoPi - heading) * axes[0]) + (-Math.cos (heading) * axes[3])) * 10;
 
-   camera.position.x += delta * axes[0] * 10;
-   camera.position.y += delta * axes[3] * 10;
-   camera.position.z += delta * axes[1] * 10;
-
-//   camera.lookAt(scene.position);
-
-   renderer.render(scene, camera);
+   renderer.render (scene, camera);
 
    lasttimer = timer;
 }
